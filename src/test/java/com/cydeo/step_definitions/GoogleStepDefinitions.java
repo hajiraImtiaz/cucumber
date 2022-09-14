d012546bd10319764bfdee184c96d2d594d23577
hajiraImtiaz <hajiradani315@gmail.com>
2022-09-14 10:39:29 +0200

    day 16 GoogleStepDefinitions

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -10,15 +10,22 @@ import org.openqa.selenium.Keys;
 public class GoogleStepDefinitions {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types {word} and clicks enter")
+    public void user_types_and_clicks_enter2(String searchKeyword) {
+
+        googleSearchPage.searchBox.sendKeys(searchKeyword);
+    }
+
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
 
         googleSearchPage.searchBox.sendKeys(searchKeyword);
     }
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
 
-        String expectedTitle = "apple - Google Search";
+        String expectedTitle = string+ " - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
         //junit assertion accepts first arg as expected, second arg as actual
@@ -30,9 +37,9 @@ public class GoogleStepDefinitions {
     public void user_types_apple_and_clicks_enter() {
     googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-        String expectedTitle = "apple - Google Search";
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String word) {
+        String expectedTitle = word+" - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
         //junit assertion accepts first arg as expected, second arg as actual
