1581b77b693cd944435200e4a442af256bdb4d50
hajiraImtiaz <hajiradani315@gmail.com>
2022-09-15 09:09:31 +0200

    day 16 WikiSearchPage.java

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -1,29 +1,39 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WikiSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Wiki_StepDefinitions {
 
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
     @Given("User is on Wikipedia home page")
     public void userIsOnWikipediaHomePage() {
         Driver.getDriver().get("https://www.wikipedia.org");
     }
 
     @When("User types Steve Jobs in the wiki search box")
-    public void userTypesSteveJobsInTheWikiSearchBox() {
+    public void userTypesSteveJobsInTheWikiSearchBox(String string) {
 
+        wikiSearchPage.searchBox.sendKeys(string);
     }
 
 
     @And("User click wiki search button")
     public void userClickWikiSearchButton() {
+       wikiSearchPage.searchBox.click();
+
     }
 
     @Then("User sees Steve Jobs is in wiki title")
-    public void userSeesSteveJobsIsInWikiTitle() {
+    public void userSeesSteveJobsIsInWikiTitle(String string) {
+
+        //verify actual title contains string (coming from feature file)
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
+
     }
 }
