0a3d0f94cfd40f40a47dc46908cace0adceb2bbe
hajiraImtiaz <hajiradani315@gmail.com>
2022-09-20 20:42:40 +0200

    day 17 WebTableLoginPage

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -18,4 +18,19 @@ public class WebTableLoginPage {
 
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
+
+    /*
+    -no parameters
+    -when we call this method, it will directly login using:
+    -username:test
+    -password:tester
+     */
+    public void login(){
+
+        this.inputUsername.sendKeys("Test");
+        this.inputPassword.sendKeys("Tester");
+        this.loginButton.click();
+
+    }
+
 }
